4fb110b107d48fe524b64623c3dc353d3df53f22
AdamSpeight2008 <Adam.Speight@Virgin.net>
2016-04-24 03:25:05 +0100

    Added in the initial UnitTests for C# Escape Sequences. The simple escape sequences have been done Primary Test for Compound Escaped Sequences begun.

--- FSDv2/Tokens/Common/Common.vb
+++ FSDv2/Tokens/Common/Common.vb
@@ -201,143 +201,6 @@
 
         End Class
 
-        Public Class SeqHead : Inherits Token
-          Friend Sub New(Span As Source.Span, Optional Inner As Tokens = Nothing)
-            MyBase.New(TokenKind.Esc_Seq_Head, Span, Inner)
-          End Sub
-        End Class
-
-        Public MustInherit Class Sequence : Inherits Token
-          Friend Sub New(Kind As TokenKind, Span As Source.Span, Optional Inner As Tokens = Nothing)
-            MyBase.New(Kind, Span, Inner)
-          End Sub
-
-          Public Shared Function TryParse(Ix As Source.Position) As Token
-            If Ix.Source.Kind <> Source.SourceKind.CS_Standard Then Return ParseError.Unsupported(Ix, $"{Ix.Source.Kind} doesn't support escape sequences")
-            If Ix.IsInvalid Then Return ParseError.EoT(Ix)
-            Dim Txn = Tokens.Empty
-            If Ix <> "\"c Then Return ParseError.NullParse(Ix)
-            Dim nx = Ix.Next
-            If nx.IsInvalid Then Return Nothing
-            Dim T As Token
-            Select Case nx
-              Case "'"c, """"c, "\"c, "0"c, "a"c, "b"c, "f"c, "n"c, "r"c, "t"c, "v"c
-                Return New Simple(Ix.To(nx.Next))
-              Case "x"c
-                T = HexaDecimal.TryParse(Ix)
-
-              Case "u"c
-                T = Unicode.TryParse(Ix)
-            End Select
-            Return Nothing
-          End Function
-
-          Public Class Simple : Inherits Esc.Sequence
-            Friend Sub New(Span As Source.Span, Optional Inner As Tokens = Nothing)
-              MyBase.New(TokenKind.Esc_Seq_Simple, Span, Inner)
-            End Sub
-          End Class
-
-          Public Class Unicode : Inherits Esc.Sequence
-
-            Friend Sub New(Span As Source.Span, Optional Inner As Tokens = Nothing)
-              MyBase.New(TokenKind.Esc_Seq_Unicode, Span, Inner)
-            End Sub
-
-            Private Shared Function Backslash_UpperU(Ix As Source.Position) As Token
-              '
-              ' unicode_escape_sequence ::= \U hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit
-              '
-              If Ix.Source.Kind <> Source.SourceKind.CS_Standard Then Return ParseError.Unsupported(Ix, $"{Ix.Source.Kind} doesn't support escape sequences")
-              If Ix.IsInvalid Then Return ParseError.EoT(Ix) '  Already at the end of the text.
-              If Ix <> "\"c Then Return ParseError.NullParse(Ix) ' Character is not a blackslash
-              Dim sx = Ix ' Start Index of this potential token.
-              Dim txn = Tokens.Empty : Ix = Ix.Next
-              If Ix.IsInvalid Then Return ParseError.NullParse(sx) ' A lone backslash (\) at the end of the text.
-              If Ix <> "U"c Then Return ParseError.NullParse(sx) ' Doesn't have correct the start to an Unicode escape squence. (\U)
-              Ix = Ix.Next
-              Dim T As Token = New Esc.SeqHead(sx.To(Ix)) : txn += T
-              ' OK. At this stage we hace the start of a escape sequence for a unicode characeter \u
-              '
-              ' unicode_escape_sequence ::= \U hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit
-              Dim Hx = Tokens.Empty
-              Dim count = 0
-              While Ix.IsInvalid AndAlso count < 8
-                T = Common.HexDigit.TryParse(Ix)
-                If T.Kind = TokenKind.ParseError Then Exit While
-                Hx = Common.AddThenNext(T, Hx, Ix)
-                count += 1
-              End While
-              If count <> 8 AndAlso count <> 0 Then Return ParseError.NullParse(sx)
-              Return New Esc.Sequence.Unicode(sx.To(Ix), txn + New HexDigits(Hx.First.Span.Start.To(Hx.Last.Span.Next), Hx))
-            End Function
-
-            Private Shared Function Backslash_LowerU(Ix As Source.Position) As Token
-              '
-              ' unicode_escape_sequence ::= \u hex_digit hex_digit hex_digit hex_digit
-              '
-              If Ix.Source.Kind <> Source.SourceKind.CS_Standard Then Return ParseError.Unsupported(Ix, $"{Ix.Source.Kind} doesn't support escape sequences")
-              If Ix.IsInvalid Then Return ParseError.EoT(Ix) ' Already at the end of the text.
-              If Ix <> "\"c Then Return ParseError.NullParse(Ix) ' Character is not a blackslash
-              Dim sx = Ix ' Start Index of this potential token.
-              Dim txn = Tokens.Empty : Ix = Ix.Next
-              If Ix.IsInvalid Then Return ParseError.NullParse(sx) ' A lone backslash (\) at the end of the text.
-              If Ix <> "u"c Then Return ParseError.NullParse(sx) ' Doesn't have correct the start to an Unicode escape squence. (\c)
-              Ix = Ix.Next
-              Dim T As Token = New Esc.SeqHead(sx.To(Ix)) : txn += T
-              ' OK. At this stage we hace the start of a escape sequence for a unicode characeter \u
-              '
-              If Ix.IsInvalid Then ParseError.NullParse(sx)
-              ' Try and parse 4 HexaDecimal characters.
-              Dim Hx = Tokens.Empty
-              Dim count = 0
-              While Ix.IsInvalid AndAlso count < 4
-                T = Common.HexDigit.TryParse(Ix)
-                If T.Kind = TokenKind.ParseError Then Exit While
-                Hx = Common.AddThenNext(T, Hx, Ix)
-                count += 1
-              End While
-              If count <> 4 AndAlso count <> 0 Then Return ParseError.NullParse(sx)
-              Return New Esc.Sequence.Unicode(sx.To(Ix), txn + New HexDigits(Hx.First.Span.Start.To(Hx.Last.Span.Next), Hx))
-            End Function
-
-            Public Shared Shadows Function TryParse(Ix As Source.Position) As Token
-              Dim T As Token
-              T = Backslash_UpperU(Ix) : If T.Kind = TokenKind.Backslash_UpperU Then Return T
-              T = Backslash_LowerU(Ix) : If T.Kind = TokenKind.Backslash_LowerU Then Return T
-              Return ParseError.NullParse(Ix)
-            End Function
-          End Class
-
-          Public Class HexaDecimal : Inherits Esc.Sequence
-            Friend Sub New(Span As Source.Span, Optional Inner As Tokens = Nothing)
-              MyBase.New(TokenKind.Esc_Seq_Unicode, Span, Inner)
-            End Sub
-            Public Shared Shadows Function TryParse(Ix As Source.Position) As Token
-              If Ix.Source.Kind <> Source.SourceKind.CS_Standard Then Return ParseError.Unsupported(Ix, $"{Ix.Source.Kind} doesn't support escape sequences")
-              If Ix.IsInvalid Then Return ParseError.EoT(Ix)
-              If Ix <> "\"c Then Return ParseError.NullParse(Ix)
-              Dim sx = Ix, txn = Tokens.Empty : Ix = Ix.Next
-              If Ix.IsInvalid Then Return ParseError.EoT(Ix) ' A lone backslash (\) at the end of the text.
-              If Ix <> "x"c Then Return ParseError.NullParse(sx) ' Doesn't have correct the start to an Unicode escape squence. (\c)
-              Ix = Ix.Next
-              Dim T As Token = New Esc.SeqHead(sx.To(Ix))
-              txn += T
-              If Ix.IsInvalid Then Return ParseError.EoT(Ix)
-              Dim Hx = Tokens.Empty
-              Dim count = 0
-              While Ix.IsInvalid AndAlso count < 4
-                T = Common.HexDigit.TryParse(Ix)
-                If T.Kind = TokenKind.ParseError Then Exit While
-                Hx = Common.AddThenNext(T, Hx, Ix)
-                count += 1
-              End While
-              If count = 0 Then Return ParseError.NullParse(sx)
-              Return New Esc.Sequence.HexaDecimal(sx.To(Ix), txn + New HexDigits(Hx.First.Span.Start.To(Hx.Last.Span.Next), Hx))
-            End Function
-          End Class
-
-        End Class
 
       End Class
 
@@ -349,6 +212,148 @@
       Return Tx
     End Function
 
-  End Class
+    Public Class Esc
 
-End Class
+      Public Class SeqHead : Inherits Token
+        Friend Sub New(Span As Source.Span, Optional Inner As Tokens = Nothing)
+          MyBase.New(TokenKind.Esc_Seq_Head, Span, Inner)
+        End Sub
+      End Class
+
+      Public MustInherit Class Sequence : Inherits Token
+        Friend Sub New(Kind As TokenKind, Span As Source.Span, Optional Inner As Tokens = Nothing)
+          MyBase.New(Kind, Span, Inner)
+        End Sub
+
+        Public Shared Function TryParse(Ix As Source.Position) As Token
+          If Ix.Source.Kind <> Source.SourceKind.CS_Standard Then Return ParseError.Unsupported(Ix, $"{Ix.Source.Kind} doesn't support escape sequences")
+          If Ix.IsInvalid Then Return ParseError.EoT(Ix)
+          Dim Txn = Tokens.Empty
+          If Ix <> "\"c Then Return ParseError.NullParse(Ix)
+          Dim nx = Ix.Next
+          If nx.IsInvalid Then Return ParseError.EoT(Ix)
+          Dim T As Token
+          Select Case nx
+            Case "'"c, """"c, "\"c, "0"c, "a"c, "b"c, "f"c, "n"c, "r"c, "t"c, "v"c
+              Return New Simple(Ix.To(nx.Next))
+            Case "x"c
+              T = HexaDecimal.TryParse(Ix)
+
+            Case "u"c, "U"c
+              T = Unicode.TryParse(Ix)
+          End Select
+          Return Nothing
+        End Function
+
+        Public Class Simple : Inherits Esc.Sequence
+          Friend Sub New(Span As Source.Span, Optional Inner As Tokens = Nothing)
+            MyBase.New(TokenKind.Esc_Seq_Simple, Span, Inner)
+          End Sub
+        End Class
+
+        Public Class Unicode : Inherits Esc.Sequence
+
+          Friend Sub New(Span As Source.Span, Optional Inner As Tokens = Nothing)
+            MyBase.New(TokenKind.Esc_Seq_Unicode, Span, Inner)
+          End Sub
+
+          Private Shared Function Backslash_UpperU(Ix As Source.Position) As Token
+            '
+            ' unicode_escape_sequence ::= \U hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit
+            '
+            If Ix.Source.Kind <> Source.SourceKind.CS_Standard Then Return ParseError.Unsupported(Ix, $"{Ix.Source.Kind} doesn't support escape sequences")
+            If Ix.IsInvalid Then Return ParseError.EoT(Ix) '  Already at the end of the text.
+            If Ix <> "\"c Then Return ParseError.NullParse(Ix) ' Character is not a blackslash
+            Dim sx = Ix ' Start Index of this potential token.
+            Dim txn = Tokens.Empty : Ix = Ix.Next
+            If Ix.IsInvalid Then Return ParseError.NullParse(sx) ' A lone backslash (\) at the end of the text.
+            If Ix <> "U"c Then Return ParseError.NullParse(sx) ' Doesn't have correct the start to an Unicode escape squence. (\U)
+            Ix = Ix.Next
+            Dim T As Token = New Esc.SeqHead(sx.To(Ix)) : txn += T
+            ' OK. At this stage we hace the start of a escape sequence for a unicode characeter \u
+            '
+            ' unicode_escape_sequence ::= \U hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit hex_digit
+            Dim Hx = Tokens.Empty
+            Dim count = 0
+            While Ix.IsInvalid AndAlso count < 8
+              T = Common.HexDigit.TryParse(Ix)
+              If T.Kind = TokenKind.ParseError Then Exit While
+              Hx = Common.AddThenNext(T, Hx, Ix)
+              count += 1
+            End While
+            If count <> 8 AndAlso count <> 0 Then Return ParseError.NullParse(sx)
+            Return New Esc.Sequence.Unicode(sx.To(Ix), txn + New HexDigits(Hx.First.Span.Start.To(Hx.Last.Span.Next), Hx))
+          End Function
+
+          Private Shared Function Backslash_LowerU(Ix As Source.Position) As Token
+            '
+            ' unicode_escape_sequence ::= \u hex_digit hex_digit hex_digit hex_digit
+            '
+            If Ix.Source.Kind <> Source.SourceKind.CS_Standard Then Return ParseError.Unsupported(Ix, $"{Ix.Source.Kind} doesn't support escape sequences")
+            If Ix.IsInvalid Then Return ParseError.EoT(Ix) ' Already at the end of the text.
+            If Ix <> "\"c Then Return ParseError.NullParse(Ix) ' Character is not a blackslash
+            Dim sx = Ix ' Start Index of this potential token.
+            Dim txn = Tokens.Empty : Ix = Ix.Next
+            If Ix.IsInvalid Then Return ParseError.NullParse(sx) ' A lone backslash (\) at the end of the text.
+            If Ix <> "u"c Then Return ParseError.NullParse(sx) ' Doesn't have correct the start to an Unicode escape squence. (\c)
+            Ix = Ix.Next
+            Dim T As Token = New Esc.SeqHead(sx.To(Ix)) : txn += T
+            ' OK. At this stage we hace the start of a escape sequence for a unicode characeter \u
+            '
+            If Ix.IsInvalid Then ParseError.NullParse(sx)
+            ' Try and parse 4 HexaDecimal characters.
+            Dim Hx = Tokens.Empty
+            Dim count = 0
+            While Ix.IsInvalid AndAlso count < 4
+              T = Common.HexDigit.TryParse(Ix)
+              If T.Kind = TokenKind.ParseError Then Exit While
+              Hx = Common.AddThenNext(T, Hx, Ix)
+              count += 1
+            End While
+            If count <> 4 AndAlso count <> 0 Then Return ParseError.NullParse(sx)
+            Return New Esc.Sequence.Unicode(sx.To(Ix), txn + New HexDigits(Hx.First.Span.Start.To(Hx.Last.Span.Next), Hx))
+          End Function
+
+          Public Shared Shadows Function TryParse(Ix As Source.Position) As Token
+            Dim T As Token
+            T = Backslash_UpperU(Ix) : If T.Kind = TokenKind.Backslash_UpperU Then Return T
+            T = Backslash_LowerU(Ix) : If T.Kind = TokenKind.Backslash_LowerU Then Return T
+            Return ParseError.NullParse(Ix)
+          End Function
+
+        End Class
+
+        Public Class HexaDecimal : Inherits Esc.Sequence
+          Friend Sub New(Span As Source.Span, Optional Inner As Tokens = Nothing)
+            MyBase.New(TokenKind.Esc_Seq_Unicode, Span, Inner)
+          End Sub
+          Public Shared Shadows Function TryParse(Ix As Source.Position) As Token
+            If Ix.Source.Kind <> Source.SourceKind.CS_Standard Then Return ParseError.Unsupported(Ix, $"{Ix.Source.Kind} doesn't support escape sequences")
+            If Ix.IsInvalid Then Return ParseError.EoT(Ix)
+            If Ix <> "\"c Then Return ParseError.NullParse(Ix)
+            Dim sx = Ix, txn = Tokens.Empty : Ix = Ix.Next
+            If Ix.IsInvalid Then Return ParseError.EoT(Ix) ' A lone backslash (\) at the end of the text.
+            If Ix <> "x"c Then Return ParseError.NullParse(sx) ' Doesn't have correct the start to an Unicode escape squence. (\c)
+            Ix = Ix.Next
+            Dim T As Token = New Esc.SeqHead(sx.To(Ix))
+            txn += T
+            If Ix.IsInvalid Then Return ParseError.EoT(Ix)
+            Dim Hx = Tokens.Empty
+            Dim count = 0
+            While Ix.IsInvalid AndAlso count < 4
+              T = Common.HexDigit.TryParse(Ix)
+              If T.Kind = TokenKind.ParseError Then Exit While
+              Hx = Common.AddThenNext(T, Hx, Ix)
+              count += 1
+            End While
+            If count = 0 Then Return ParseError.NullParse(sx)
+            Return New Esc.Sequence.HexaDecimal(sx.To(Ix), txn + New HexDigits(Hx.First.Span.Start.To(Hx.Last.Span.Next), Hx))
+          End Function
+        End Class
+      End Class
+
+    End Class
+
+    End Class
+
+  End Class
---
+++ FSDv2_UnitTests/Tokens/Common/EscSeq_Simple_Tests.vb
@@ -0,0 +1,152 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports FSDv2
+Imports FSDv2.FormatString.Common
+<TestClass> Public Class EscSeq_Simple_Tests
+
+  Const Cat = "Tokens.Common.Esc.Seq.Simple"
+
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _00_()
+    Dim Text = "\'"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.VB_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsInstanceOfType(res, GetType(ParseError))
+    Assert.AreEqual(ParseError.Reason.Unsupported, DirectCast(res, ParseError).Why)
+  End Sub
+
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _01_()
+    Dim Text = "\'"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Verbatum)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsInstanceOfType(res, GetType(ParseError))
+    Assert.AreEqual(ParseError.Reason.Unsupported, DirectCast(res, ParseError).Why)
+  End Sub
+
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _02_()
+    Dim Text = "\'"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsNotInstanceOfType(res, GetType(ParseError))
+    Assert.IsInstanceOfType(res, GetType(Esc.Sequence.Simple))
+    Assert.AreEqual("(  0:  2)", res.Span.ToString)
+  End Sub
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _03_()
+    Dim Text = "\""" ' \"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsNotInstanceOfType(res, GetType(ParseError))
+    Assert.IsInstanceOfType(res, GetType(Esc.Sequence.Simple))
+    Assert.AreEqual("(  0:  2)", res.Span.ToString)
+  End Sub
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _04_()
+    Dim Text = "\\"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsNotInstanceOfType(res, GetType(ParseError))
+    Assert.IsInstanceOfType(res, GetType(Esc.Sequence.Simple))
+    Assert.AreEqual("(  0:  2)", res.Span.ToString)
+  End Sub
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _05_()
+    Dim Text = "\0"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsNotInstanceOfType(res, GetType(ParseError))
+    Assert.IsInstanceOfType(res, GetType(Esc.Sequence.Simple))
+    Assert.AreEqual("(  0:  2)", res.Span.ToString)
+  End Sub
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _06_()
+    Dim Text = "\a"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsNotInstanceOfType(res, GetType(ParseError))
+    Assert.IsInstanceOfType(res, GetType(Esc.Sequence.Simple))
+    Assert.AreEqual("(  0:  2)", res.Span.ToString)
+  End Sub
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _07_()
+    Dim Text = "\b"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsNotInstanceOfType(res, GetType(ParseError))
+    Assert.IsInstanceOfType(res, GetType(Esc.Sequence.Simple))
+    Assert.AreEqual("(  0:  2)", res.Span.ToString)
+  End Sub
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _08_()
+    Dim Text = "\f"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsNotInstanceOfType(res, GetType(ParseError))
+    Assert.IsInstanceOfType(res, GetType(Esc.Sequence.Simple))
+    Assert.AreEqual("(  0:  2)", res.Span.ToString)
+  End Sub
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _09_()
+    Dim Text = "\n"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsNotInstanceOfType(res, GetType(ParseError))
+    Assert.IsInstanceOfType(res, GetType(Esc.Sequence.Simple))
+    Assert.AreEqual("(  0:  2)", res.Span.ToString)
+  End Sub
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _10_()
+    Dim Text = "\r"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsNotInstanceOfType(res, GetType(ParseError))
+    Assert.IsInstanceOfType(res, GetType(Esc.Sequence.Simple))
+    Assert.AreEqual("(  0:  2)", res.Span.ToString)
+  End Sub
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _11_()
+    Dim Text = "\t"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsNotInstanceOfType(res, GetType(ParseError))
+    Assert.IsInstanceOfType(res, GetType(Esc.Sequence.Simple))
+    Assert.AreEqual("(  0:  2)", res.Span.ToString)
+  End Sub
+  <TestMethod, TestCategory(Cat)>
+  Public Sub _12_()
+    Dim Text = "\v"
+    Dim TheSource = Source.Create(Text, Source.SourceKind.CS_Standard)
+    Dim FirstPos = TheSource.First
+    Dim res = FormatString.Common.Esc.Sequence.TryParse(FirstPos)
+    Assert.IsNotNull(res)
+    Assert.IsNotInstanceOfType(res, GetType(ParseError))
+    Assert.IsInstanceOfType(res, GetType(Esc.Sequence.Simple))
+    Assert.AreEqual("(  0:  2)", res.Span.ToString)
+  End Sub
+End Class
