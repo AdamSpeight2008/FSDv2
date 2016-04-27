7a0232401da38a35e0bc210481605bfc5c0c5566
AdamSpeight2008 <Adam.Speight@Virgin.net>
2016-04-27 07:45:21 +0100

    Tidy up of the Text Token

--- FSDv2/Tokens/FormatString/Text/Text.vb
+++ FSDv2/Tokens/FormatString/Text/Text.vb
@@ -14,41 +14,45 @@
       Dim Txn = Tokens.Empty, sx = Ix, T As Token
       Dim TextStart As New Source.Position?
       While Ix.IsValid
+#Region " Check to see if it is an escaped Brace Closing or Openind"
         T = Common.Brace.Esc.Closing.TryParse(Ix) : If T.Kind = TokenKind.Esc_Brace_Closing Then Txn = Common.AddThenNext(T, Txn, Ix, TextStart) : Continue While
         T = Common.Brace.Esc.Opening.TryParse(Ix) : If T.Kind = TokenKind.Esc_Brace_Opening Then Txn = Common.AddThenNext(T, Txn, Ix, TextStart) : Continue While
+#End Region
+#Region "Is it a Brace Closing } ?"
         T = Common.Brace.Closing.TryParse(Ix)
         If T.Kind = TokenKind.Brace_Closing Then
           If ParsingArgFormatText Then Exit While
           Txn = Common.AddThenNext(ParseError.Make.Invalid(T.Span, T, ""), Txn, Ix, TextStart)
         End If
+#End Region
+#Region "Is it a Brace Opeining { ?"
         T = Common.Brace.Opening.TryParse(Ix)
         If T.Kind = TokenKind.Brace_Opening Then
           If ParsingArgFormatText Then Txn = Common.AddThenNext(ParseError.Make.Invalid(T.Span, T, ""), Txn, Ix, TextStart) : Continue While
           Txn = Common.AddThenNext(Nothing, Txn, Ix, TextStart) : Exit While
-
         End If
-        If Ix = "\"c Then
-          If Ix.Source.Kind = Source.SourceKind.CS_Standard Then
-            T = Common.Esc.Sequence.TryParse(Ix)
-            Select Case T.Kind
-              Case TokenKind.Esc_Seq_Simple, TokenKind.Esc_Seq_HexaDecimal, TokenKind.Esc_Seq_Unicode
-                Txn = Common.AddThenNext(T, Txn, Ix, TextStart) : Continue While
-              Case TokenKind.Partial
-                Txn = Common.AddThenNext(T, Txn, Ix, TextStart) : Continue While
-              Case TokenKind.ParseError
-                If T.IsNotNullParse Then Txn = Common.AddThenNext(T, Txn, Ix, TextStart) : Continue While
-            End Select
-          End If
-
+#End Region
+#Region "Check to see it is a C# specific escape sequence"
+        If Ix.Source.Kind = Source.SourceKind.CS_Standard AndAlso Ix = "\"c Then
+          T = Common.Esc.Sequence.TryParse(Ix)
+          Select Case T.Kind
+            Case TokenKind.Esc_Seq_Simple, TokenKind.Esc_Seq_HexaDecimal, TokenKind.Esc_Seq_Unicode
+              Txn = Common.AddThenNext(T, Txn, Ix, TextStart) : Continue While
+            Case TokenKind.Partial
+              Txn = Common.AddThenNext(T, Txn, Ix, TextStart) : Continue While
+            Case TokenKind.ParseError
+              If T.IsNotNullParse Then Txn = Common.AddThenNext(T, Txn, Ix, TextStart) : Continue While
+          End Select
         End If
+#End Region
+#Region "Otherwise treat it as just a text character."
         If TextStart Is Nothing Then TextStart = New Source.Position?(Ix)
         Ix = Ix.Next
+#End Region
       End While
       If TextStart IsNot Nothing Then Txn = Common.AddThenNext(Nothing, Txn, Ix, TextStart)
       Return New Text(sx.To(Ix), Txn)
     End Function
-
-
 
   End Class
 
