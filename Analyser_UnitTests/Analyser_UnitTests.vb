e338e6935e48c1c17e4eec5815db84c905090700
AdamSpeight2008 <Adam.Speight@Virgin.net>
2016-04-30 21:56:54 +0100

    Added initial UnitTests for the Analyser.

---
+++ Analyser_UnitTests/Analyser_UnitTests.vb
@@ -0,0 +1,224 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports FSDv2
+Imports System.Runtime.CompilerServices
+
+Public Module Exts
+
+  <Extension>
+  Public Function AsString(Ix As FSDv2_Analyser.Analyser.Issues) As String
+    If Ix Is Nothing Then Return "{Nothing}"
+    Dim sb As New Text.StringBuilder
+    For Each i In Ix.Issues
+      sb.AppendLine($"{i.Span.ToString} {i.Kind.ToString}")
+    Next
+    Return sb.ToString
+  End Function
+
+
+End Module
+
+<TestClass()>
+Public Class FSDv2_UnitTests
+
+  Const Cat0 = "Analyser (CS_Standard)"
+
+  <TestMethod, TestCategory(Cat0)>
+  Public Sub _00_EmptyString()
+    Dim TheText = ""
+    Dim TheSource = Source.Create(TheText, Source.SourceKind.CS_Standard)
+    Dim ParseResult = FormatString.TryParse(TheSource.First.Value)
+    Dim Analyser As New FSDv2_Analyser.Analyser()
+    Dim Parameters As New FSDv2_Analyser.Analyser.Parameters()
+    Dim Result = Analyser.Analyse(ParseResult, Parameters)
+    Dim Text = Result.Result.Issues.AsString
+    Dim Expected = ""
+    Assert.AreEqual(Expected, Text)
+  End Sub
+
+  <TestMethod, TestCategory(Cat0)>
+  Public Sub _01_JustText()
+    Dim TheText = "abc"
+    Dim TheSource = Source.Create(TheText, Source.SourceKind.CS_Standard)
+    Dim ParseResult = FormatString.TryParse(TheSource.First.Value)
+    Dim Analyser As New FSDv2_Analyser.Analyser()
+    Dim Parameters As New FSDv2_Analyser.Analyser.Parameters()
+    Dim Result = Analyser.Analyse(ParseResult, Parameters)
+    Dim Text = Result.Result.Issues.AsString
+    Dim Expected = ""
+    Assert.AreEqual(Expected, Text)
+
+  End Sub
+
+  '  <TestMethod, TestCategory(Cat0)>
+  '  Public Sub _02_Text_Brace_Closing()
+  '    Dim TheText = "}"
+  '    Dim TheSource = Source.Create(TheText, Source.SourceKind.CS_Standard)
+  '    Dim ParseResult = FormatString.TryParse(TheSource.First.Value)
+  '    Dim Text = ParseResult.AsString()
+  '    Dim Expected =
+  '"(  0:  1)  FormatString
+  '  [ 0]  (  0:  1)  ParseError.Invalid
+  '    [ 0]  (  0:  1)  Brace_Closing
+  '"
+  '    Assert.AreEqual(Expected, Text)
+  '  End Sub
+
+  '  <TestMethod, TestCategory(Cat0)>
+  '  Public Sub _03_Text_Brace_Opening()
+  '    Dim TheText = "{"
+  '    Dim TheSource = Source.Create(TheText, Source.SourceKind.CS_Standard)
+  '    Dim ParseResult = FormatString.TryParse(TheSource.First.Value)
+  '    Dim Text = ParseResult.AsString()
+  '    Dim Expected =
+  '"(  0:  1)  FormatString
+  '  [ 0]  (  0:  1)  ArgHole
+  '    [ 0]  (  0:  1)  Brace_Opening
+  '    [ 1]  (  1:  0)  ParseError.EoT
+  '"
+  '    Assert.AreEqual(Expected, Text)
+  '  End Sub
+
+  '  <TestMethod, TestCategory(Cat0)>
+  '  Public Sub _04_EscapedOpening()
+  '    Dim TheText = "{{"
+  '    Dim TheSource = Source.Create(TheText, Source.SourceKind.CS_Standard)
+  '    Dim ParseResult = FormatString.TryParse(TheSource.First.Value)
+  '    Dim Text = ParseResult.AsString()
+  '    Dim Expected =
+  '"(  0:  2)  FormatString
+  '  [ 0]  (  0:  2)  Esc_Brace_Opening
+  '    [ 0]  (  0:  1)  Brace_Opening
+  '    [ 1]  (  1:  1)  Brace_Opening
+  '"
+  '    Assert.AreEqual(Expected, Text)
+  '  End Sub
+
+  '  <TestMethod, TestCategory(Cat0)>
+  '  Public Sub _05_EscapedClosing()
+  '    Dim TheText = "}}"
+  '    Dim TheSource = Source.Create(TheText, Source.SourceKind.CS_Standard)
+  '    Dim ParseResult = FormatString.TryParse(TheSource.First.Value)
+  '    Dim Text = ParseResult.AsString()
+  '    Dim Expected =
+  '"(  0:  2)  FormatString
+  '  [ 0]  (  0:  2)  Esc_Brace_Closing
+  '    [ 0]  (  0:  1)  Brace_Closing
+  '    [ 1]  (  1:  1)  Brace_Closing
+  '"
+  '    Assert.AreEqual(Expected, Text)
+  '  End Sub
+
+  '  <TestMethod, TestCategory(Cat0)>
+  '  Public Sub _06_EmptyArgHole()
+  '    Dim TheText = "{}"
+  '    Dim TheSource = Source.Create(TheText, Source.SourceKind.CS_Standard)
+  '    Dim ParseResult = FormatString.TryParse(TheSource.First.Value)
+  '    Dim Text = ParseResult.AsString()
+  '    Dim Expected =
+  '"(  0:  2)  FormatString
+  '  [ 0]  (  0:  2)  ArgHole
+  '    [ 0]  (  0:  1)  Brace_Opening
+  '    [ 1]  (  1:  0)  ParseError.Partial
+  '      [ 0]  (  1:  1)  Brace_Closing
+  '    [ 2]  (  1:  1)  Brace_Closing
+  '"
+  '    Assert.AreEqual(Expected, Text)
+  '  End Sub
+
+  '  <TestMethod, TestCategory(Cat0)>
+  '  Public Sub _07_EmptyArgHoles()
+  '    Dim TheText = "{}{}"
+  '    Dim TheSource = Source.Create(TheText, Source.SourceKind.CS_Standard)
+  '    Dim ParseResult = FormatString.TryParse(TheSource.First.Value)
+  '    Dim Text = ParseResult.AsString()
+  '    Dim Expected =
+  '"(  0:  4)  FormatString
+  '  [ 0]  (  0:  2)  ArgHole
+  '    [ 0]  (  0:  1)  Brace_Opening
+  '    [ 1]  (  1:  0)  ParseError.Partial
+  '      [ 0]  (  1:  1)  Brace_Closing
+  '    [ 2]  (  1:  1)  Brace_Closing
+  '  [ 1]  (  2:  2)  ArgHole
+  '    [ 0]  (  2:  1)  Brace_Opening
+  '    [ 1]  (  3:  0)  ParseError.Partial
+  '      [ 0]  (  3:  1)  Brace_Closing
+  '    [ 2]  (  3:  1)  Brace_Closing
+  '"
+  '    Assert.AreEqual(Expected, Text)
+  '  End Sub
+  '  <TestMethod, TestCategory(Cat0)>
+  '  Public Sub _08_()
+  '    '              0123456
+  '    Dim TheText = " {} {} "
+  '    Dim TheSource = Source.Create(TheText, Source.SourceKind.CS_Standard)
+  '    Dim ParseResult = FormatString.TryParse(TheSource.First.Value)
+  '    Dim Text = ParseResult.AsString()
+  '    Dim Expected =
+  '"(  0:  7)  FormatString
+  '  [ 0]  (  0:  1)  Text
+  '  [ 1]  (  1:  2)  ArgHole
+  '    [ 0]  (  1:  1)  Brace_Opening
+  '    [ 1]  (  2:  0)  ParseError.Partial
+  '      [ 0]  (  2:  1)  Brace_Closing
+  '    [ 2]  (  2:  1)  Brace_Closing
+  '  [ 2]  (  3:  1)  Text
+  '  [ 3]  (  4:  2)  ArgHole
+  '    [ 0]  (  4:  1)  Brace_Opening
+  '    [ 1]  (  5:  0)  ParseError.Partial
+  '      [ 0]  (  5:  1)  Brace_Closing
+  '    [ 2]  (  5:  1)  Brace_Closing
+  '  [ 4]  (  6:  1)  Text
+  '"
+  '    Assert.AreEqual(Expected, Text)
+  '  End Sub
+  '  <TestMethod, TestCategory(Cat0)>
+  '  Public Sub _09_()
+  '    '              0123456
+  '    Dim TheText = "}} {} {} {{"
+  '    Dim TheSource = Source.Create(TheText, Source.SourceKind.CS_Standard)
+  '    Dim ParseResult = FormatString.TryParse(TheSource.First.Value)
+  '    Dim Text = ParseResult.AsString()
+  '    Dim Expected =
+  '"(  0: 11)  FormatString
+  '  [ 0]  (  0:  2)  Esc_Brace_Closing
+  '    [ 0]  (  0:  1)  Brace_Closing
+  '    [ 1]  (  1:  1)  Brace_Closing
+  '  [ 1]  (  2:  1)  Text
+  '  [ 2]  (  3:  2)  ArgHole
+  '    [ 0]  (  3:  1)  Brace_Opening
+  '    [ 1]  (  4:  0)  ParseError.Partial
+  '      [ 0]  (  4:  1)  Brace_Closing
+  '    [ 2]  (  4:  1)  Brace_Closing
+  '  [ 3]  (  5:  1)  Text
+  '  [ 4]  (  6:  2)  ArgHole
+  '    [ 0]  (  6:  1)  Brace_Opening
+  '    [ 1]  (  7:  0)  ParseError.Partial
+  '      [ 0]  (  7:  1)  Brace_Closing
+  '    [ 2]  (  7:  1)  Brace_Closing
+  '  [ 5]  (  8:  1)  Text
+  '  [ 6]  (  9:  2)  Esc_Brace_Opening
+  '    [ 0]  (  9:  1)  Brace_Opening
+  '    [ 1]  ( 10:  1)  Brace_Opening
+  '"
+  '    Assert.AreEqual(Expected, Text)
+  '  End Sub
+
+  '  <TestMethod, TestCategory(Cat0)>
+  '  Public Sub _10_()
+  '    '              0123456
+  '    Dim TheText = "{x}"
+  '    Dim TheSource = Source.Create(TheText, Source.SourceKind.CS_Standard)
+  '    Dim ParseResult = FormatString.TryParse(TheSource.First.Value)
+  '    Dim Text = ParseResult.AsString()
+  '    Dim Expected =
+  '"(  0:  3)  FormatString
+  '  [ 0]  (  0:  3)  ArgHole
+  '    [ 0]  (  0:  1)  Brace_Opening
+  '    [ 1]  (  1:  1)  ParseError.Partial
+  '      [ 0]  (  2:  1)  Brace_Closing
+  '    [ 2]  (  2:  1)  Brace_Closing
+  '"
+  '    Assert.AreEqual(Expected, Text)
+  '  End Sub
+End Class
--- FSDv2 Analyser/Analyser.vb
+++ FSDv2 Analyser/Analyser.vb
@@ -81,16 +81,26 @@ Public Class Analyser
 
   End Class
 
-  Public Function Analyse(FS As FSDv2.FormatString, q As Parameters) As Parameters
-    For Each t As Token In FS.InnerTokens.GetEnumerator
-      Select Case t.Kind
-        Case TokenKind.ArgHole : q = ArgHole(DirectCast(t, FormatString.ArgHole), q)
-        Case TokenKind.Text : q = Text(DirectCast(t, FormatString.Text), q)
-        Case TokenKind.ParseError : q = ParseError(DirectCast(t, ParseError), q)
-        Case Else
-          q.Result.Issues += New Issue(Issue.Kinds.Unexpected_Token, t.Span, "")
-      End Select
-    Next
+  Public Function Analyse(FS As FSDv2.Token, q As Parameters) As Parameters
+    Select Case FS.Kind
+      Case TokenKind.ParseError
+        Dim pe As ParseError = DirectCast(FS, ParseError)
+        Select Case pe.Why
+          Case FSDv2.ParseError.Reason.NullParse
+          Case Else
+            q.Result.Issues += New Issue(Issue.Kinds.Unexpected_Token, pe.Span, $"Reason:= {pe.Why}")
+        End Select
+      Case TokenKind.FormatString
+        For Each t As Token In FS.InnerTokens.GetEnumerator
+          Select Case t.Kind
+            Case TokenKind.ArgHole : q = ArgHole(DirectCast(t, FormatString.ArgHole), q)
+            Case TokenKind.Text : q = Text(DirectCast(t, FormatString.Text), q)
+            Case TokenKind.ParseError : q = ParseError(DirectCast(t, ParseError), q)
+            Case Else
+              q.Result.Issues += New Issue(Issue.Kinds.Unexpected_Token, t.Span, "")
+          End Select
+        Next
+    End Select
     Return q
   End Function
 
